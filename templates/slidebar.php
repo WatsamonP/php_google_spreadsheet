7dd36dbc9731bd40c5cec6bca06fc6aea032e2c1
Heshani Hettigoda <heshanihettigoda.94@gmail.com>
2020-03-20 18:37:34 +0700

    fix wording

--- templates/slidebar.php
+++ templates/slidebar.php
@@ -1,6 +1,6 @@
 <nav id="sidebar">
   <div class="sidebar-header">
-    <h3>WSA PROJECT</h3>
+    <h3>Water Security Assessment</h3>
     <strong>WSA</strong>
   </div>
 
@@ -15,42 +15,42 @@
     <li class="nav-item">
       <a href="/php_google_spreadsheet/index.php?page=dimension_one">
         <div class="abs"><i class="fas fa-tint" style="padding-right:4px; padding-left:4px"></i> D1</div>
-        <span><i class="fas fa-tint" style="padding-right:4px; padding-left:4px"></i> Dimenstion 1</span>
+        <span><i class="fas fa-tint" style="padding-right:4px; padding-left:4px"></i> Water Availability </span>
       </a>
     </li>
 
     <li class="nav-item">
       <a href="/php_google_spreadsheet/index.php?page=dimension_two">
         <div class="abs"><i class="fas fa-truck-loading"></i> D2</div>
-        <span><i class="fas fa-truck-loading"></i> Dimenstion 2</span>
+        <span><i class="fas fa-truck-loading"></i> Water Productivity</span>
       </a>
     </li>
 
     <li class="nav-item">
       <a href="/php_google_spreadsheet/index.php?page=dimension_three">
         <div class="abs"><i class="fas fa-warehouse"></i> D3</div>
-        <span><i class="fas fa-warehouse"></i> Dimenstion 3</span>
+        <span><i class="fas fa-warehouse"></i> Water-related Disasters</span>
       </a>
     </li>
 
     <li class="nav-item">
       <a href="/php_google_spreadsheet/index.php?page=dimension_four">
         <div class="abs"><i class="fas fa-check-circle" style="padding-right:4px;"></i> D4</div>
-        <span><i class="fas fa-check-circle" style="padding-right:4px;"></i> Dimenstion 4</span>
+        <span><i class="fas fa-check-circle" style="padding-right:4px;"></i> Watershed Health</span>
       </a>
     </li>
 
     <li class="nav-item">
       <a href="/php_google_spreadsheet/index.php?page=dimension_five">
         <div class="abs"><i class="fas fa-th-list" style="padding-right:4px;"></i> D5</div>
-        <span><i class="fas fa-th-list" style="padding-right:4px;"></i> Dimenstion 5</span>
+        <span><i class="fas fa-th-list" style="padding-right:4px;"></i> Water Governance</span>
       </a>
     </li>
 
     <li class="nav-item">
       <a href="/php_google_spreadsheet/index.php?page=aggregation">
         <div class="abs"><i class="fas fa-chart-bar" style="padding-right:4px;"></i> AGG</div>
-        <span><i class="fa fa-chart-bar" style="padding-right:4px;"></i> Aggregation</span>
+        <span><i class="fa fa-chart-bar" style="padding-right:4px;"></i> Final Result</span>
       </a>
     </li>
   </ul>
